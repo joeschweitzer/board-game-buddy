52c2d5c967ee56567a0136991f05500f5c73033a
joeschweitzer <joeschweitzer@b05fd8c0-233f-11de-a78b-a7aca27b2395>
2009-04-16 03:55:32 +0000

    Initial checkin for cmstructure and initial files for python bgb and examples

---
+++ trunk/src/python/examples/tictactoe/tictactoe.py
@@ -0,0 +1,20 @@
+##Tic-tac-toe, also spelled tick tack toe, and
+##alternatively called noughts and crosses, hugs and kisses,
+##and many other names, is a pencil-and-paper game for two players,
+##O and X, who take turns marking the spaces in a 33 grid,
+##usually X going first. The player who succeeds in placing
+##three respective marks in a horizontal, vertical or diagonal
+##row wins the game.
+
+# Set up two players
+# Set up game board
+# Set up basic turn algorithm
+# Set up basic round algorithm
+# Valid moves - where empty space
+# Game over? - all spaces are filled or someone won
+# Victory conditions - 3 in a row
+# Set up basic who goes first - random?
+# Output/Print out board
+# Input for moves index?
+
+import bgb
