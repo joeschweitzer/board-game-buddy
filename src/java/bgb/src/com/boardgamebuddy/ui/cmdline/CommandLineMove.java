759327a435a1e1c5553f526f0474511ca9a851e1
joeschweitzer <joeschweitzer@b05fd8c0-233f-11de-a78b-a7aca27b2395>
2009-09-19 17:22:53 +0000

    Starting UI work for command line

---
+++ src/java/bgb/src/com/boardgamebuddy/ui/cmdline/CommandLineMove.java
@@ -0,0 +1,53 @@
+package com.boardgamebuddy.ui.cmdline;
+
+/**
+ * Move input by command line
+ */
+public class CommandLineMove {
+
+	private String player;
+	private String piece;
+	private String space;
+	
+	/**
+	 * Get player
+	 */
+	public final String getPlayer() {
+		return player;
+	}
+	
+	/**
+	 * Set player
+	 */
+	public final void setPlayer(final String playerIn) {
+		this.player = playerIn;
+	}
+	
+	/**
+	 * Get piece
+	 */
+	public final String getPiece() {
+		return piece;
+	}
+	
+	/**
+	 * Set piece
+	 */
+	public final void setPiece(final String pieceIn) {
+		this.piece = pieceIn;
+	}
+	
+	/**
+	 * Get space
+	 */
+	public final String getSpace() {
+		return space;
+	}
+	
+	/**
+	 * Set space
+	 */
+	public final void setSpace(final String spaceIn) {
+		this.space = spaceIn;
+	}
+}
